0108745f1f3e41f301a36c671784750ff22de0c0
Rafael Lima <rafael_lima1998@hotmail.com>
2024-10-01 18:42:49 +0000

    finalizada dim_produtos

--- models/staging/erp/stg_erp__categorias.sql
+++ models/staging/erp/stg_erp__categorias.sql
@@ -1,9 +1,11 @@
 with
-    fonte_categorias as (
-        select *
+    renomeacao as (
+        select
+            cast(ID as int) as pk_categoria
+            , cast(CATEGORYNAME as varchar) as nome_categoria
+            , cast(DESCRIPTION as varchar) as descricao_categoria
         from {{ source('erp_northwind', 'category') }}
-
     )
 
 select *
-from fonte_categorias
+from renomeacao
